1c3a952bd91219cdb6d20b6fc5622105950e78a1
darian847 <daryanrehat@gmail.com>
2025-04-14 17:22:52 +0700

    14/4/24

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,8 @@
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        Halaman Utama
+        <x-search-bar></x-search-bar>
+        {{-- <x-html-print><x-label></x-label></x-html-print> --}}
 
       </div>
     </main>
